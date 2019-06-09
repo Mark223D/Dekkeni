91896856aa25dae884aaad8642f736744b05e7d3
Mark Debbane <markdebbane@gmail.com>
2019-06-10 00:58:13 +0300

    Updated

--- ECommerce/ecommerce/settings/production.py
+++ ECommerce/ecommerce/settings/production.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'm9*hh44m!9ofdnro)s01bb=3$i&ho5#55&q2pfq1%d_zyc(s-x'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.herokuapp.com']
+ALLOWED_HOSTS = ['dekkeni.herokuapp.com']
 
 
 # Application definition

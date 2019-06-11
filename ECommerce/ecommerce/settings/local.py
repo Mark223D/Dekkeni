2eaddf73dbbccc3eec1e39ed613ac9b0144f5bbe
Mark Debbane <markdebbane@gmail.com>
2019-06-11 21:52:27 +0300

    testing on local config with allowed host

--- ECommerce/ecommerce/settings/local.py
+++ ECommerce/ecommerce/settings/local.py
@@ -25,8 +25,7 @@ SECRET_KEY = 'm9*hh44m!9ofdnro)s01bb=3$i&ho5#55&q2pfq1%d_zyc(s-x'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['','dekkeni.herokuapp.com']
 
 # Application definition
 
